312c4eec4ef014396fddde146d713b7f5871da6a
Chris Jamieson <jamiesoncj@gmail.com>
2021-03-16 19:34:47 +0000

    Different approach to external lib

--- vue.config.js
+++ vue.config.js
@@ -4,9 +4,6 @@ module.exports = {
       filename: "index.js",
       libraryTarget: "umd",
     },
-    externals: {
-      "vue-star-rating": "commonjs2 vue-star-rating",
-    },
     mode: "production",
   },
 };
